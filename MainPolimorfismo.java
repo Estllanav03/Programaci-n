a1a576ae0128b7311a1ba400e92ef1089c4c6595
Estllanav03 <120552640+Estllanav03@users.noreply.github.com>
2023-01-23 10:16:25 +0100

    Ejercicio 4 - Polimorfismo

---
+++ MainPolimorfismo.java
@@ -0,0 +1,85 @@
+
+
+
+/*public class MainPolimorfismo {
+
+    public static void main(String[] args){
+
+
+
+    }
+}
+
+class Coche {
+
+    private String modelo;
+    private boolean motor;
+    private int ruedas;
+    private int cilindros;
+
+    public Coche(String modelo /*int cilindros*/)
+
+        /*  this.modelo = modelo;
+        this.cilindros = cilindros;
+        this.ruedas = 4;
+        this.motor = true;
+    }
+
+    public String getModelo(String modelo) {
+        return modelo;
+    }
+
+    public int getCilindros(int cilindros) {
+        return cilindros;
+    }
+
+    public void arrancar() {
+        System.out.println("arrancar el coche");
+    }
+
+    public void acelerar() {
+        System.out.println("acelerar el coche");
+    }
+
+    public void frenar() {
+        System.out.println("frenar el coche");
+    }
+
+    public String marca(){
+        return "No es una marca";
+    }
+
+}*/
+
+  /*  class Mercedes extends Coche {
+        public Mercedes() {
+         super("Mercedes");
+    }
+
+    @Override
+    public String marca() {
+        return "Mercedes es una marca de coches.";
+    }
+}
+
+    class Ferrari extends Coche {
+        public Ferrari() {
+         super("Ferrari");
+    }
+
+    @Override
+    public String marca() {
+        return "Ferrari es una marca de coches.";
+    }
+}
+
+    class Ford extends Coche {
+        public Ford() {
+           super("Ford");
+        }
+    @Override
+    public String marca() {
+            return "Ford es una marca de coches.";
+    }
+
+    }*/
